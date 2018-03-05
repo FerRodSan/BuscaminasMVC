8920e9d1e24c9889ed06a24ed428c7c16bd0b819
Fernan <36297944+FerRodSan@users.noreply.github.com>
2018-03-05 13:05:22 +0100

    texto de fallos en principal

--- src/Control/Principal.java
+++ src/Control/Principal.java
@@ -22,6 +22,13 @@ public class Principal {
 	}
 }
 
+//   FALLOS:
 // problema con la recursividad
 // el reinicio me abre nueva ventana
 // al pisar mina se me queda en blanco el cuadrado
+// al pulsar una casilla desvelada hay que desvelar las de su alrededor, no lo hace
+// meter dificultad
+// meter el tamaño del lado del tablero
+
+
+
